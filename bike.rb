0e3490a5f76fac27fb7ef2a0dfd8eee1d42c5f44
SamsonDeVol <devols@oregonstate.edu>
2021-11-14 18:47:23 -0800

    add principles from POODR

--- bike.rb
+++ bike.rb
@@ -2,30 +2,41 @@
 
 class Bike
 
-  STANDARD_WEIGHT = 200 # lbs
-  MAX_CARGO_ITEMS = 10
+  STANDARD_WEIGHT = 200 # lb
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
+  attr_accessor :id, :color, :price, :weight, :rented, :cargo
 
-  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
+  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false, cargo)
     @id = id
     @color = color
     @price = price
     @weight = weight
     @rented = rented
-    @cargo_contents = []
+    @cargo = cargo
   end
 
   def rent!
-    self.rented = true
+    rented = true
   end
 
+end
+
+class Cargo
+
+  MAX_CARGO_ITEMS = 10
+
+  attr_accessor :cargo_contents
+
+  def initialize(cargo_contents = [])
+    @cargo_contents = cargo_contents
+  end 
+
   def add_cargo(item)
-    self.cargo_contents << item
+    cargo_contents << item
   end
 
   def remove_cargo(item)
-    self.cargo_contents.remove(item)
+    cargo_contents.remove(item)
   end
 
   def pannier_capacity
@@ -33,7 +44,7 @@ class Bike
   end
 
   def pannier_remaining_capacity
-    MAX_CARGO_ITEMS - self.cargo_contents.size
+    MAX_CARGO_ITEMS - cargo_contents.size
   end
 
 end
